e4b2d2bd28c699df00a5f560431e2ded69f30960
ins429@gmail.com <ins429@gmail.com>
2019-05-02 17:54:32 -0700

    wip

--- family.js
+++ family.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 
-const baseUrl = 'http://ins429.ddns.net:60429/family/images'
+// const baseUrl = 'http://ins429.ddns.net:60429/family/images'
+const baseUrl = 'http://localhost:60429/family/images'
 const buildImgUrl = filename => `${baseUrl}/${filename}`
 
 const Family = () => {
-  const [images, setImages] = useState(['yes'])
+  const [images, setImages] = useState([])
   useEffect(() => {
     const fetchImages = async () => {
       const {
@@ -26,11 +27,16 @@ const Family = () => {
       Peter's Family
       <div>
         {images.map(img => (
-          <a key={img} href={buildImgUrl(img)}>
-            <img width="200px" src={buildImgUrl(img)} alt={img} />
-          </a>
+          <Fragment>
+            <a key={img} href={buildImgUrl(img)}>
+              <img width="200px" src={buildImgUrl(img)} alt={img} />
+            </a>
+            <form action={buildImgUrl(img) + '/delete'}>
+              <input type="submit" value="delete" />
+            </form>
+          </Fragment>
         ))}
-        <form action={baseUrl} method="POST">
+        <form action={baseUrl} method="POST" enctype="multipart/form-data">
           <input type="text" name="filename" />
           <input type="file" name="file" />
           <input type="submit" value="submit" />
